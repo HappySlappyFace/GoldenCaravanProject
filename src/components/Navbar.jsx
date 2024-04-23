c6b5fb482024f4620ef824804e01258c17c19f70
HappySlappyFace <11047926+HappySlappyFace@users.noreply.github.com>
2024-04-23 15:05:12 +0100

    Updated navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,39 @@
 import "./Navbar.scss";
 function Navbar() {
   return (
-    <>
-      <div className="is-flex navbar">
-        <img className="is-flex-grow-1" src="/Navbar.png" alt="" />
+    <nav className="navbar" role="navigation" aria-label="main navigation">
+      <div className="navbar-brand">
+        <a className="navbar-item" href="/">
+          <strong>Golden Caravan</strong>
+        </a>
+
+        {/* Here's the hamburger menu which is hidden on desktop and visible on mobile */}
+        <a
+          role="button"
+          className="navbar-burger"
+          aria-label="menu"
+          aria-expanded="false"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
       </div>
-    </>
+
+      <div className="navbar-menu">
+        <div className="navbar-end">
+          <a className="navbar-item" href="/book-room">
+            Book a room
+          </a>
+          <a className="navbar-item" href="/favorites">
+            Favorites
+          </a>
+          <a className="navbar-item" href="/login">
+            Login
+          </a>
+        </div>
+      </div>
+    </nav>
   );
 }
 
--- src/components/Rooms/RoomDetailsRenderer.jsx
+++ src/components/Rooms/RoomDetailsRenderer.jsx
@@ -7,8 +7,8 @@ function RoomDetailsRenderer(props) {
           <div className="column is-one-third">
             <figure className="image is-4by3">
               <img
-                src={roomDetails.imageUrl}
-                alt={`Room ${roomDetails.idRoom}`}
+                src={props.roomDetails.imageUrl}
+                alt={`Room ${props.roomDetails.idRoom}`}
                 style={{ objectFit: "cover" }}
               />
             </figure>
@@ -16,14 +16,15 @@ function RoomDetailsRenderer(props) {
 
           {/* Information Column */}
           <div className="column">
-            <p className="title is-4">{roomDetails.roomType} Room</p>
-            <p className="subtitle is-6">Room ID: {roomDetails.idRoom}</p>
-            <p>Number of Beds: {roomDetails.numberOfBeds}</p>
-            <p>Price per Night: ${roomDetails.price}</p>
+            <p className="title is-4">{props.roomDetails.roomType} Room</p>
+            <p className="subtitle is-6">Room ID: {props.roomDetails.idRoom}</p>
+            <p>Number of Beds: {props.roomDetails.numberOfBeds}</p>
+            <p>Price per Night: ${props.roomDetails.price}</p>
             <p>
-              Status: {roomDetails.status === 1 ? "Available" : "Unavailable"}
+              Status:{" "}
+              {props.roomDetails.status === 1 ? "Available" : "Unavailable"}
             </p>
-            <p>Hotel ID: {roomDetails.idHotel}</p>
+            <p>Hotel ID: {props.roomDetails.idHotel}</p>
             <time dateTime="2016-1-1">Last updated: 11:09 PM - 1 Jan 2016</time>
           </div>
         </div>
